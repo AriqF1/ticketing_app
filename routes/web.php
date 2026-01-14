5a9e0f5ab80a6a2b5633f984f62e344942cb556b
Farhan Ariq <122168324+AriqF1@users.noreply.github.com>
2026-01-14 20:31:08 +0700

    Add resource routes for categories, events, tickets, and histories in admin panel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\EventController;
+use App\Http\Controllers\Admin\TiketController;
+use App\Http\Controllers\Admin\HistoriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +19,15 @@ Route::middleware('auth')->group(function () {
 
     Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+        Route::resource('categories', CategoryController::class);
+
+        Route::resource('events', EventController::class);
+
+        Route::resource('tickets', TiketController::class);
+
+        Route::get('/histories', [HistoriesController::class, 'index'])->name('histories.index');
+        Route::get('/histories/{id}', [HistoriesController::class, 'show'])->name('histories.show');
     });
 });
 
