e2f07b48b2c7705e4077bfc126088922caeb2a30
Ariq Farhan <ariqalthaf445@gmail.com>
2026-01-15 22:15:29 +0700

    Refactor event show page for improved structure and readability; update ticket history link and enhance checkout modal functionality

--- resources/views/components/user/navigation.blade.php
+++ resources/views/components/user/navigation.blade.php
@@ -33,21 +33,21 @@
 
                 </div>
                 <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                <li>
-                    <a href="#" class="justify-between">
-                        Riwayat Pembelian
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('logout') }}"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <li>
+                        <a href="{{ route('orders.index') }}" class="justify-between">
+                            Riwayat Pembelian
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             Logout
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                        @csrf
-                    </form>
-                </li>
-            </ul>
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
             </div>
         @endauth
 
